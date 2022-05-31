f4c6fd041037b959ad2caffb4b1f5a0cd5cc77fe
Reinaldo44 <reinaldoap82@hotmail.com>
2022-05-30 23:49:49 -0300

    first commit

---
+++ src/main/java/com/DigitalInovation/Apipadroesprojetospring/ApiPadroesProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.DigitalInovation.Apipadroesprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPadroesProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DigitalInovation/Apipadroesprojetospring/ApiPadroesProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DigitalInovation.Apipadroesprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPadroesProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
